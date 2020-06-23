2cecff6a51cd04efbdaceeb74930020bae5a649d
lminggvick <lminggvick@naver.com>
2020-06-23 10:11:39 +0900

    Add: security ignore matcher 설정

--- src/main/java/toy/spring/ssl/config/WebSecurityConfig.java
+++ src/main/java/toy/spring/ssl/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package toy.spring.ssl.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -35,6 +36,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .logout()
                 .logoutSuccessUrl("/home")
                 .permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/css/**", "/script/**", "/image/**", "/fonts/**", "/lib/**");
     }
 
     /**
